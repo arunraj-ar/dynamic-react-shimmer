631e61322f4013a04c34dcaa59ed4bf15f16fb3f
arunraj-ar <mail.arunrajar@gmail.com>
2024-09-17 19:05:30 +0530

    added shimmer through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import MagicShimmer from './MagicShimmer'
+import "./App.css";
+import MagicShimmer from "./MagicShimmer";
 
 function App() {
-
   return (
     <>
-      <MagicShimmer/>
+      <MagicShimmer
+        layout={[
+          { shape: "rectangle", width: "100%", height: "20px", margin:"10px" },
+          { shape: "circle", width: "50px", height: "50px", margin:"10px"},
+          { shape: "rectangle", width: "80%", height: "15px", margin:"10px"},
+        ]}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/MagicShimmer.jsx
+++ src/MagicShimmer.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./MagicShimmer.css";
 
-const MagicShimmer = () => {
-  return <div className="min-h-dvh flex justify-center items-center">MagicShimmer</div>;
+const MagicShimmer = (props) => {
+  const {
+    shape = "rectangle",
+    width = "100%",
+    height = "20px",
+    animate = true,
+    speed = 2,
+    theme = "dark",
+    layout = [],
+  } = props;
+
+  const layoutStructure = layout.map((item, index) => (
+    <div
+      key={index}
+      className={`hole  ${animate ? "animate" : ""}`}
+      style={{
+        width: item.width || "100%",
+        height: item.height || "20px",
+        margin: item.margin || "10px",
+      }}
+    />
+  ));
+  return (
+    <div className="back flex justify-center items-center min-w-max">
+      <div className="base">
+        {layoutStructure.length > 0 && layoutStructure}
+      </div>
+    </div>
+  );
 };
 
 export default MagicShimmer;
